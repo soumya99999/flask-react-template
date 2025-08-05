aefee8e34110292dcf4092ae965dff4a298e6339
Punith <110555829+PunithKosana@users.noreply.github.com>
2025-08-05 13:15:17 +0530

    fix: temporal deployment in preview and upgrade ci version (#285)
    * fix: temporal deployment in preview
    
    * upgrade ci version to v3.2.3

--- lib/kube/scripts/post-deploy.sh
+++ lib/kube/scripts/post-deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 kubectl rollout status deploy/"$KUBE_APP"-deployment -n "$KUBE_NS"
-
-# TODO: https://github.com/jalantechnologies/flask-react-template/issues/274
-# Temporary workaround to avoid Temporal rollout check failure in preview env
-if [[ "$KUBE_ENV" != "preview" ]]; then
-  kubectl rollout status deploy/"$KUBE_APP"-temporal-deployment -n "$KUBE_NS"
-else
-  echo "post-deploy :: skipping Temporal rollout check in preview environment"
-fi
+kubectl rollout status deploy/"$KUBE_APP"-temporal-deployment -n "$KUBE_NS"
